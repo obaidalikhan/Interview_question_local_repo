2171102c617b765a62934c08b1aaa25ff5d73bc5
Obaid Khan <mobaidalikhan@hotmail.com>
2026-05-28 22:26:29 -0400

    Display Array, ArrayList and List

--- Interview_question_local_repo/Interview_question_local_repo/Array_ArrayList_List.cs
+++ Interview_question_local_repo/Interview_question_local_repo/Array_ArrayList_List.cs
@@ -7,20 +7,42 @@ using System.Threading.Tasks;
 
 namespace Arrays
 {
-    internal class Array_ArrayList_List
+    public class Array_ArrayList_List
     {
-        public Array_ArrayList_List()
+        public static void Run()
         {
             // Array
             int[] array = new int[3] { 1, 2, 3 };
+
+            // Display array
+            Console.WriteLine("Array: " + string.Join(", ", array));
 
             // ArrayList
             ArrayList arrayList = new ArrayList();
             arrayList.Add(1);
             arrayList.Add("hello");            // Not type-safe
+            Console.WriteLine("ArrayList: " + string.Join(", ", arrayList.ToArray()));
+            // Example: iterate
+            Console.Write("ArrayList items: ");
+            foreach (var item in arrayList)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
 
             // List<T>
             List<int> genericList = new List<int> { 1, 2, 3 };
+            // Display List<T>
+            Console.WriteLine("List<int>: " + string.Join(", ", genericList));
+
+            Console.Write("List<int> items: ");
+            foreach (var n in genericList)
+            {
+                Console.Write(n + " ");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
 
         }
     }
