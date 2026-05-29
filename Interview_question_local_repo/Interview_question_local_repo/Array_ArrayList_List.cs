e68813c0b8fb6c03c0ff80b28ef97f3cc19f73d3
Obaid Khan <mobaidalikhan@hotmail.com>
2026-05-28 22:14:29 -0400

    Array, Arraylist and List use

---
+++ Interview_question_local_repo/Interview_question_local_repo/Array_ArrayList_List.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Array_ArrayList_List
+    {
+        public Array_ArrayList_List()
+        {
+            // Array
+            int[] array = new int[3] { 1, 2, 3 };
+
+            // ArrayList
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(1);
+            arrayList.Add("hello");            // Not type-safe
+
+            // List<T>
+            List<int> genericList = new List<int> { 1, 2, 3 };
+        }
+    }
+}
